c732d67d24a0f3e375bce25deeec92c70da77d31
Techzaro <techxaro@gmail.com>
2026-09-18 17:35:21 +0500

    Fixes are made in latests

---
+++ frontend/dist/assets/useApi-CpHcYDa-.js
@@ -0,0 +1,1 @@
+import{jn as e,rt as t}from"./index-Bysl_Hrz.js";function n(n,r,i,a={}){return e({queryKey:Array.isArray(n)?n:[n,i],queryFn:()=>t.get(r,i),...a})}export{n as t};
--- frontend/dist/assets/useApi-e00dJiiU.js
+++
@@ -1,1 +0,0 @@
-import{jn as e,rt as t}from"./index-Cg7_ue4k.js";function n(n,r,i,a={}){return e({queryKey:Array.isArray(n)?n:[n,i],queryFn:()=>t.get(r,i),...a})}export{n as t};
